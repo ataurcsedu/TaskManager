36ec7dc0b7196342389af11a50c50b1d270051ca
Tahmim Ahmed Shibli <shibli05@gmail.com>
2018-01-17 17:57:18 +0600

    db parameters.

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,12 @@
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
-const sequelize = new Sequelize('taskmanager', 'root', 'civs', {
-  host: '192.168.103.18',
+
+const db_user = process.env.MYSQL_USER || 'root'
+const db_password = process.env.MYSQL_PASSOWRD || 'civs'
+const db_host = process.env.MYSQL_HOST || '192.168.103.18'
+
+const sequelize = new Sequelize('taskmanager', db_user, db_password, {
+  host: db_host,
   dialect: 'mysql',
   define: {
         timestamps: false
