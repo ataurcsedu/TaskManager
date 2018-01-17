8caeb3a1b52adc62e0e8ce4da98b7667dc9d638d
Tahmim Ahmed Shibli <shibli05@gmail.com>
2018-01-17 17:10:13 +0600

    localhost

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ app.post('/create-user',function(req,res,next){
 // Binding express app to port 3000
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '192.168.103.18'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.listen(server_port,server_ip_address,function(){
     console.log('Node server running @' +server_ip_address+':'+server_port)
